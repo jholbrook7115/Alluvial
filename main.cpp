f348737a342e670bd4186c94bdb297c1f5dd33e2
Mitchell McCann <tud46355@temple.edu>
2015-04-12 13:01:08 -0400

    Added Connections for slots/signals and removed unnecessary Settings code that had been commented out.

--- main.cpp
+++ main.cpp
@@ -6,69 +6,74 @@
 #include <QDebug>
 #include <QUrl>
 #include "settings_storage.h"
+#include "mediaplayer.h"
 #include <QtQml>
-
-/*
-Settings::Settings(QObject *parent): QObject(parent){
-
-}
-
-void Settings::setValue(const QString &key, const QVariant &value){
-    client_settings.setValue(key, value);
-}
-QVariant Settings::value(const QString &key, const QVariant &defaultValue) const{
-    return client_settings.value(key, defaultValue);
-}
-*/
+#include <QMediaPlayer>
+#include "playlist.h"
 
 int main(int argc, char *argv[])
 {
-
     qmlRegisterSingletonType(QUrl("qrc:/GlobalVars.qml"), "Alluvial.Globals", 1, 0, "Globals");
-    qmlRegisterType<Settings_storing>("AlluvialSettings", 0, 1, "ClientSettings");
-
 
 	QApplication app(argc, argv);
-
     QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
     app.setOrganizationName("AlluvialAlphaBuild");
-    app.setOrganizationDomain("couponbug.com");
     app.setApplicationName("Alluvial");
 
     Settings_storing *settings = new Settings_storing();
-
-
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     engine.rootContext()->setContextProperty("clientSettings", settings);
 
-    /*
-     * IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     * ("../Alluvial/main.qml")
-     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     * IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    QQuickView *view = new QQuickView;
-    QScopedPointer<QApplication> application(new QApplication(argc, argv));
-    //QScopedPointer<QmlApplicationViewer> viewer(QmlApplicationViewer::create());
-    Settings* settings = new Settings(application.data());
-    QUrl url("file:///home/jefferson/Code/Alluvial/Alluvial");
+    mediaPlayer *mp = new mediaPlayer();
+    playlist *ph = new playlist("Playlist 1");
 
+    QObject *root = engine.rootObjects().first();
+    QObject *playButton = root->findChild<QObject*>("playButton");
+    QObject *playbackSlider = root->findChild<QObject*>("playbackSlider");
+    QObject *volSlider = root->findChild<QObject*>("volumeSlider");
+    QObject *rwButton = root->findChild<QObject*>("rewindButton");
+    QObject *ffButton = root->findChild<QObject*>("fastForwardButton");
+    QObject *leftSkip = root->findChild<QObject*>("leftSkipButton");
+    QObject *rightSkip = root->findChild<QObject*>("rightSkipButton");
+    QObject *shufButton = root->findChild<QObject*>("shuffleButton");
+    QObject *repButton = root->findChild<QObject*>("repeatButton");
 
-    view->rootContext()->setContextProperty("settings", settings);
-    view->setSource();
-    view->showNormal();
+    // Pause or play the song
+    QObject::connect(playButton, SIGNAL(playClicked()),
+        mp, SLOT(playOrPause()));
 
-   // engine = new QQmlEngine;
-   // QQmlEngine *qmlengine = new QQmlEngine;
-   // qmlengine->rootContext()->setContextProperty("settings", settings);
-   //  component = new QQmlComponent(qmlengine);
+    // Adjust the volume according to the position of the volume slider
+    QObject::connect(volSlider, SIGNAL(changeVol(int)),
+        mp, SLOT(setVolume(int)));
 
+    // Skip back - NEEDS TO BE FIXED TO HANDLE HOLD DOWN
+    QObject::connect(rwButton, SIGNAL(clicked()),
+        mp, SLOT(rewind()));
 
+    // Skip ahead - NEEDS TO BE FIXED TO HANDLE HOLD DOWN
+    QObject::connect(ffButton, SIGNAL(clicked()),
+        mp, SLOT(fastForward()));
 
-    QObject *topLevel = engine.rootObjects().value(0);
-    QQuickWindow *window = qobject_cast<QQuickWindow *>(topLevel);
+    // Skip to position based off of the slider
+    QObject::connect(playbackSlider, SIGNAL(playbackPosChanged(int)),
+        mp, SLOT(skipTo(int)));
 
-    window->show();
-*/
+    // Skip to position based off of the slider
+    QObject::connect(playbackSlider, SIGNAL(playbackPosChanged(int)),
+        mp, SLOT(skipTo(int)));
+
+    //QObject::connect(shufButton, SIGNAL(clicked()),
+    //    ph, SLOT(shuffleButton()));
+
+    //QObject::connect(repButton, SIGNAL(clicked()),
+    //    ph, SLOT(repeatButton()));
+
+    //QObject::connect(leftSkip, SIGNAL(clicked()),
+    //    ph, SLOT(previousSong()));
+
+    //QObject::connect(rightSkip, SIGNAL(clicked()),
+    //    ph, SLOT(nextSong()));
+
     return app.exec();
 }
